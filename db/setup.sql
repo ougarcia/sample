5c849ab2d5bb93b0efe43f9d6ce836d96913e1f3
ougarcia <oz.ulysses@gmail.com>
2015-10-10 17:14:23 -0700

    first commit

---
+++ db/setup.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS todos;
+CREATE TABLE todos (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+);
+
+INSERT INTO
+  todos (title)
+VALUES
+  ('Laundry'),
+  ('Buy Groceries'),
+  ('Water Plants'),
+  ('Run Dishwasher'),
+  ('Walk the Dog'),
+  ('Clean Room');
